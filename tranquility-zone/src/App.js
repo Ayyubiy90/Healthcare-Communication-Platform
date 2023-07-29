d58b05111b857a05d12a61b77035d10037ceb0cd
ayb <ayyubiy67@gmail.com>
2023-07-29 04:50:00 +0100

    cretaed some file like writing session

--- tranquility-zone/src/App.js
+++ tranquility-zone/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import WritingSession from './components/WritingSession';
+import RegistrationForm from './components/RegistrationForm';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to handle user login
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  // Function to handle user logout
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      {!isLoggedIn ? (
+        <>
+          <RegistrationForm />
+          <LoginForm onLogin={handleLogin} />
+        </>
+      ) : (
+        <>
+          <WritingSession />
+          <button onClick={handleLogout} className="logout-button">Logout</button>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ tranquility-zone/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">TranquilityZone</div>
+      {/* Add navigation links or buttons here if needed */}
+    </nav>
+  );
+};
+
+export default Navbar;
