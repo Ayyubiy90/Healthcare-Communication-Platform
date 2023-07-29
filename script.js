ee27695a7438ef03416c49a00c53536354da2f8e
ayb <ayyubiy67@gmail.com>
2023-07-29 05:54:57 +0100

    add some improvements and error handling to the front-end code for a more robust real-time messaging feature

--- script.js
+++ script.js
@@ -53,22 +53,62 @@ function displayMessage(message, sender, timestamp) {
   chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
+// Function to handle errors and display error messages
+function displayError(message) {
+  const div = document.createElement("div");
+  div.classList.add("error-message");
+  div.textContent = message;
+  chatMessages.appendChild(div);
+}
+
 // WebSocket connection (to be implemented in the backend)
-const socket = io(); // Replace 'io()' with the actual WebSocket connection URL
+const socket = new WebSocket("ws://localhost:8080");
+
+// Event listener when the WebSocket connection is established
+socket.addEventListener("open", () => {
+  console.log("WebSocket connection established.");
+});
+
+// Event listener for WebSocket connection errors
+socket.addEventListener("error", (event) => {
+  console.error("WebSocket connection error:", event);
+  displayError("Error: Failed to establish WebSocket connection.");
+});
+
+// Event listener for WebSocket connection closed
+socket.addEventListener("close", (event) => {
+  console.warn("WebSocket connection closed:", event);
+  displayError(
+    "WebSocket connection closed. Please refresh the page to reconnect."
+  );
+});
 
 // Listen for incoming messages
-socket.on("message", (data) => {
-  const { message, sender } = data;
-  displayMessage(message, sender);
-});
+// socket.on("message", (data) => {
+//   const { message, sender } = data;
+//   displayMessage(message, sender);
+// });
 
 // Send message when the form is submitted
 chatForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const messageInput = document.getElementById("message-input");
-  const message = messageInput.value;
+  const message = messageInput.value.trim();
 
-  // Emit the message to the WebSocket server (to be implemented in the backend)
-  socket.emit("sendMessage", { message });
+  if (message === "") {
+    displayError("Error: Message cannot be empty.");
+    return;
+  }
+
+  // Emit the message to the WebSocket server
+  socket.send(JSON.stringify({ message }));
   messageInput.value = ""; // Clear the input field after sending the message
 });
+
+// Function to format timestamp for messages
+function formatTimestamp() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  return `${hours}:${minutes}`;
+}
