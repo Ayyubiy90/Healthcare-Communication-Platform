3a6e6e32f2868f2c2be2adb6c0f9fc68ac3196d6
ayb <ayyubiy67@gmail.com>
2023-07-29 05:23:38 +0100

    Implemented header and welcome section

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+// Add your JavaScript code for front-end functionality here
+
+// Example: Handle a form submission
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // Process the form data and submit to the server
+});
